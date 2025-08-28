c8a60250380f9ef0009a74c67d48d17b593a23ea
Rahul <rahul.h26@outlook.com>
2025-08-28 11:14:17 +0100

    The following four companies have been chosen

---
+++ draft_one.py
@@ -0,0 +1,6 @@
+import numpy as np 
+import matplotlib.pyplot as plt 
+import yfinance as yf 
+import datetime as datetime 
+
+print("Hello World")
--- portfolio.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd 
-import numpy as np 
-import scipy as sp  
-import matplotlib.pyplot as plt 
-
-stickers = ['GOOG', 'AAPL', 'AMZN', '']
